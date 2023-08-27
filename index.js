44f20c68a69551cea73e8c7fc5bde3a813e8833f
HammadAli55 <hammadalii@outlook.com>
2023-08-27 16:45:56 +0500

    finding habitual planets

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-const {parse} = require("csv-parse");
+const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+// const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_prad"] < 1.6
+  );
+}
 
 fs.createReadStream("kepler_data.csv")
-.pipe(parse({
-    comment: '#',
-    columns: true
-}))
-.on("data", (data) => {
-  results.push(data);
-})
-.on('end', () => {
-  console.log('err')  
-})
-.on('end', () => {
-    console.log('results', results);  
-    console.log('done');
-});
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    //   results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on("end", () => {
+    console.log("err");
+  })
+  .on("end", () => {
+    // console.log("results", results);
+    console.log(habitablePlanets)
+    console.log("done");
+  });
