3b6403e021ee6e4b50204c80dddc2b4df28821e2
HammadAli55 <hammadalii@outlook.com>
2023-08-28 14:15:35 +0500

    404 not found

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ server.on('request', (req, res) => {
         res.write('</body>')
         res.write('</html>')
         res.end();
+    } else {
+        res.statusCode = 404;
+        res.end();
     }
 })
 
