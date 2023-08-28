1b5635f335e591ad8b3c962f0a06452566eace33
HammadAli55 <hammadalii@outlook.com>
2023-08-28 14:01:38 +0500

    set routing

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
-const { parse } = require("csv-parse");
-const fs = require("fs");
+const http = require('http');
+const PORT = 3100;
+const server = http.createServer();
 
-// const results = [];
-const habitablePlanets = [];
-
-function isHabitablePlanet(planet) {
-  return (
-    planet["koi_disposition"] === "CONFIRMED" &&
-    planet["koi_insol"] > 0.36 &&
-    planet["koi_prad"] < 1.6
-  );
-}
-
-fs.createReadStream("kepler_data.csv")
-  .pipe(
-    parse({
-      comment: "#",
-      columns: true,
-    })
-  )
-  .on("data", (data) => {
-    //   results.push(data);
-    if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
+server.on('request', (req, res) => {
+    if(req.url === '/friends') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        // res.writeHead(200, {
+        //     'Content-type':'application/json'
+        // });
+        res.end(JSON.stringify({
+            id:1, name: 'shah'
+        }))
+    } else if(req.url === '/message'){
+        res.setHeader('Content-type', 'text/html')
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<ul>')
+        res.write('<li>Helli Isaava!</li>')
+        res.write('<li>Helli Isaav!</li>')
+        res.write('</ul>')
+        res.write('</body>')
+        res.write('</html>')
+        res.end();
     }
-  })
-  .on("error", () => {
-    console.log("err");
-  })
-  .on("end", () => {
-    // console.log("results", results);
-    console.log(habitablePlanets.map((planet) => {
-        return planet['kepler_name']
-    }))
-    console.log(`${habitablePlanets.length} habitable planets found`);
-  });
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+}) ; 
